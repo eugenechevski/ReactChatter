7429e5b33253b40a92899e693d6c4eab34d8d959
Eugene Chevski <eugenechevski@proton.me>
2023-12-28 14:46:19 -0500

    adds the first dev build

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 export default {
   expo: {
-    name: "ReactChatter",
+    name: "react-chatter",
     slug: "react-chatter",
     version: "1.0.0",
     orientation: "portrait",
@@ -9,34 +9,19 @@ export default {
     assetBundlePatterns: ["**/*"],
     ios: {
       supportsTablet: true,
-      googleServicesFile: process.env.GOOGLE_SERVICES_PLIST || './GoogleService-Info.plist',
       bundleIdentifier: "eugenechevski.reactchatter",
+    },
+    android: {
+      package: "eugenechevski.reactchatter"
     },
     web: {
       favicon: "./assets/images/logo.png",
     },
-    plugins: [
-      "@react-native-firebase/app",
-      "@react-native-firebase/perf",
-      "@react-native-firebase/crashlytics",
-      [
-        "expo-build-properties",
-        {
-          ios: {
-            useFrameworks: "static",
-          },
-        },
-      ],
-    ],
-    android: {
-      package: "eugenechevski.reactchatter",
-      googleServicesFile: process.env.GOOGLE_SERVICES_JSON || "./google-services.json",
-    },
+    owner: "eugenechevski",
     extra: {
       eas: {
-        projectId: "4642cea0-c9d7-4073-9b4f-8f3efaec2697",
-      },
-    },
-    owner: "eugenechevski",
+        projectId: "4642cea0-c9d7-4073-9b4f-8f3efaec2697"
+      }
+    }
   },
 };
