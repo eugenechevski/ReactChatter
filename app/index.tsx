f2a49b3bbefb18356470a751e7b7bdbde6563b46
Eugene Chevski <eugenechevski@proton.me>
2024-01-13 18:53:59 -0500

    adds the UI for the root page

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,41 @@
-import { Box, Text, Button, Input } from "native-base";
+import { VStack, Text, Image, PresenceTransition } from "native-base";
+import logo from "@/assets/images/logo-256.png";
 
 export default function RootScreen() {
   return (
-    <Box height="100%" width="100%" alignItems="center" justifyContent="center">
-      <Text>Welcome to React Chatter.</Text>
-    </Box>  
+    <VStack
+      backgroundColor={"main.sky"}
+      height="100%"
+      width="100%"
+      alignItems="center"
+      justifyContent="center"
+      space={3}
+    >
+      <PresenceTransition
+        initial={{ opacity: 0 }}
+        visible={true}
+        animate={{
+          opacity: 1,
+          transition: {
+            duration: 1000,
+          },
+        }}
+      >
+        <Image
+          source={logo}
+          alt="logo"
+          size={"2xl"}
+          alignSelf={"center"}
+        />
+        <Text
+          fontSize={"2xl"}
+          fontWeight={"bold"}
+          color={"main.crisp"}
+          shadow={3}
+        >
+          Welcome to React Chatter.
+        </Text>
+      </PresenceTransition>
+    </VStack>
   );
 }
