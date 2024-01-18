175cf09c72a27372aabf0894c3fbbe13549d81bd
Eugene Chevski <eugenechevski@proton.me>
2024-01-18 14:14:22 -0500

    adds the initial loading logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,65 +1,31 @@
-// React
-import { useState, useEffect } from "react";
-
 // NativeBase
 import { NativeBaseProvider, Box } from "native-base";
 
 // Expo
-import { Slot, useRouter } from "expo-router";
+import { Slot } from "expo-router";
 
 // Styles
 import theme from "@/styles/globalStyles";
 
-// Firebase
-import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+// Context
+import { ContextProvider } from "@/context/Context";
 
 export default function RootLayout() {
-  const router = useRouter();
-
-  /**
-   * At beginning, it is needed to be determined whether a user is signed in or not.
-   */
-  const [user, setUser] = useState<null | FirebaseAuthTypes.User>(null);
-  const [loading, setLoading] = useState(true);
-
-  const onAuthStateChanged = (user: FirebaseAuthTypes.User | null) => {
-    setUser(user);
-    setLoading(false);
-  };
-
-  // Listen for authentication state to change.
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-
-    return subscriber;
-  }, []);
-
-  useEffect(() => {
-    if (loading) return;
-
-    if (user) {
-      // User is signed in
-      console.log("User is signed in");
-    } else {
-      // User is signed out
-      console.log("User is signed out");
-      router.push("/sign-in");
-    }
-  }, [user, loading]);
-
   return (
-    <NativeBaseProvider theme={theme}>
-      <Box
-        flex={1}
-        minHeight={"full"}
-        minWidth={"full"}
-        borderWidth={2}
-        alignItems="center"
-        justifyContent="center"
-      >
-        {/** Child root */}
-        <Slot />
-      </Box>
-    </NativeBaseProvider>
+    <ContextProvider>
+      <NativeBaseProvider theme={theme}>
+        <Box
+          flex={1}
+          minHeight={"full"}
+          minWidth={"full"}
+          borderWidth={2}
+          alignItems="center"
+          justifyContent="center"
+        >
+          {/** Child root */}
+          <Slot />
+        </Box>
+      </NativeBaseProvider>
+    </ContextProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,56 @@
-import { VStack, Text, Image, PresenceTransition, Button } from "native-base";
-import logo from "@/assets/images/logo-256.png";
+// React
+import { useState, useEffect } from "react";
+
+// NativeBase
+import { VStack, Text, Image, Button } from "native-base";
+
+// Expo
 import { useRouter } from "expo-router";
+
+// Styles and assets
+import logo from "@/assets/images/logo-256.png";
+
+// Firebase
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+
+// Context
+import { useUserContext } from "@/context/user/UserContext";
 
 export default function RootScreen() {
   const router = useRouter();
+  const { dispatch } = useUserContext();
+
+  /**
+   * At beginning, it is needed to be determined whether a user is signed in or not.
+   */
+  const [user, setUser] = useState<null | FirebaseAuthTypes.User>(null);
+  const [loading, setLoading] = useState(true);
+
+  const onAuthStateChanged = (user: FirebaseAuthTypes.User | null) => {
+    setUser(user);
+    setLoading(false);
+  };
+
+  // Listen for authentication state to change.
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+
+    return subscriber;
+  }, []);
+
+  useEffect(() => {
+    if (loading) return;
+
+    if (user) {
+      // User is signed in
+      console.log("User is signed in");
+      dispatch({ type: "SET_USER", payload: user });
+    } else {
+      // User is signed out
+      console.log("User is signed out");
+      router.push("/sign-in");
+    }
+  }, [user, loading]);
 
   return (
     <VStack
