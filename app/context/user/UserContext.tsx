343adf950c0b3f34041cc30ef0bf5e9e9a16545a
Eugene Chevski <eugenechevski@proton.me>
2024-02-09 22:39:58 -0500

    adds types for UserContext

--- app/context/user/UserContext.tsx
+++ app/context/user/UserContext.tsx
@@ -1,20 +1,39 @@
-import { createContext, useContext, useReducer } from "react";
+import { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { createContext, useContext, useReducer, Dispatch } from "react";
 
-const UserContext = createContext(null);
+interface State {
+  userCredential: FirebaseAuthTypes.User;
+  userData: User;
+}
 
-const initialState = {
-  user: {} as User,
+type Action = 
+  | { type: 'SET_USER_CREDENTIAL'; payload: FirebaseAuthTypes.User }
+  | { type: 'SET_USER_DATA'; payload: User };
+
+interface UserContextProps {
+  state: State;
+  dispatch: Dispatch<Action>;
+}
+
+const UserContext = createContext<UserContextProps | null>(null);
+
+const initialState: State = {
+  userCredential: {} as FirebaseAuthTypes.User,
+  userData: {} as User
 };
 
-const userReducer = (state, action) => {
+const userReducer = (state: State, action: Action): State => {
   switch (action.type) {
-    // Receives a firebase user object
-    // Find the user in the database
-    // Sets the user object in the state
-    case "SET_USER":
+    case "SET_USER_CREDENTIAL":
       return {
         ...state,
-        user: action.payload as User,
+        userCredential: action.payload,
+      };
+
+    case "SET_USER_DATA":
+      return {
+        ...state,
+        userData: action.payload,
       };
 
     default:
@@ -32,4 +51,10 @@ export const UserProvider = ({ children }) => {
   );
 };
 
-export const useUserContext = () => useContext(UserContext);
+export const useUserContext = (): UserContextProps => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('useUserContext must be used within a UserProvider');
+  }
+  return context;
+};
